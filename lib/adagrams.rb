1eceffc83d582b2ecf100fc45775735bd3dca370
Maryam Shitu <maryamshitu@gmail.com>
2018-08-14 16:45:15 -0700

    wave2, created uses_available_letters? method

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,37 +1,32 @@
-letters_hash = {
-"A" => 9,
-"B" => 2,
-"C" => 2,
-"D" => 4,
-"E" => 12,
-"F" => 2,
-"G" => 3,
-"H" => 2,
-"I" => 9,
-"J" => 1,
-"K" => 1,
-"L" => 4,
-"M" => 2,
-"N" => 6,
-"O" => 8,
-"P" => 2,
-"Q" => 1,
-"R" => 6,
-"S" => 4,
-"T" => 6,
-"U" => 4,
-"V" => 2,
-"W" => 2,
-"X" => 1,
-"Y" => 2,
-"Z" => 1
-}
+# letters_hash = {
+# "A" => 9,
+# "B" => 2,
+# "C" => 2,
+# "D" => 4,
+# "E" => 12,
+# "F" => 2,
+# "G" => 3,
+# "H" => 2,
+# "I" => 9,
+# "J" => 1,
+# "K" => 1,
+# "L" => 4,
+# "M" => 2,
+# "N" => 6,
+# "O" => 8,
+# "P" => 2,
+# "Q" => 1,
+# "R" => 6,
+# "S" => 4,
+# "T" => 6,
+# "U" => 4,
+# "V" => 2,
+# "W" => 2,
+# "X" => 1,
+# "Y" => 2,
+# "Z" => 1
+# }
 
-
-letter_bag = []
-letters_hash.each do |letter, amount|
-  amount.times  { letter_bag << letter }
-end
 
 def draw_letters
   letters_hash = {
@@ -63,16 +58,40 @@ def draw_letters
   "Z" => 1
   }
 
-
+# creats array of 10 random letters
   letter_bag = []
-  letters_hash.each do |letter, amount|
+  letters_hash.map do |letter, amount|
     amount.times  { letter_bag << letter }
   end
 
-  
   letter_hand = Array.new(letter_bag.sample(10))
 
   return letter_hand
 end
+# displays letter hand
+letters_in_hand = draw_letters
+puts letters_in_hand
 
-p draw_letters
+# collects word from user
+print "Enter word: "
+word = gets.chomp.upcase
+
+# checks if word entered is valid
+def uses_available_letters?(input, letters_in_hand)
+flag = 0
+
+  input.split('').each do |letter|
+      if !(letters_in_hand.include?(letter))
+        flag += 1
+      end
+  end
+
+  if flag > 0
+    return false
+  else
+    return true
+  end
+
+end
+
+# puts uses_available_letters?(word, letters_in_hand)
