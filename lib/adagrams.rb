7a4137751da36b2d1beb8c7fcbfcbeb4ee057fc1
Maryam Shitu <maryamshitu@gmail.com>
2018-08-17 14:05:37 -0700

    wave 5 done and cleaned up

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -2,7 +2,9 @@ require 'pry'
 require 'csv'
 require 'awesome_print'
 
+# method to draw hand of letters
 def draw_letters
+  # { key = letter, value = quantity of letters}
   letters_hash = {
   "A" => 9,
   "B" => 2,
@@ -32,36 +34,32 @@ def draw_letters
   "Z" => 1
   }
 
-# creats array of 10 random letters
+  # create array with all available letters
   letter_bag = []
   letters_hash.map do |letter, amount|
     amount.times  { letter_bag << letter }
   end
-
+  # random draw from available letters
   letter_hand = Array.new(letter_bag.sample(10))
-
   return letter_hand
 end
 
 # checks if word entered is valid
 def uses_available_letters?(input, letters_in_hand)
 flag = 0
+# use dup to make a temporary copy of original hand
 copy_letters_hand = letters_in_hand.dup
-
-  input.upcase.split('').each do |letter|
+# split user word into array of single letters
+# when word is valid remove letters used from copy hand
+input.upcase.split('').each do |letter|
     if !(copy_letters_hand.include?(letter))
       flag += 1
     else
       copy_letters_hand.delete_at(copy_letters_hand.index letter)
     end
   end
-
-  if flag > 0
-    return false
-  else
-    return true
-  end
-
+  # return true if letters are all valid in hand, else -> false
+  return flag > 0 ? false : true
 end
 
 # asign scores to letters
@@ -94,41 +92,31 @@ def score_word(word)
     "Y" => 4,
     "Z" => 10
   }
-
-score = 0
-  word.upcase.split("").each do |letter|
-    score +=  score_chart[letter]
-  end
-
-  if word.length > 6 && word.length < 11
-    score += 8
-  end
- return score
+  # letter points based on value in hash
+  score = 0
+  word.upcase.split("").each { |letter| score +=  score_chart[letter] }
+  # 7 to 10 letter words get 8 extra points
+  score += 8 if word.length > 6 && word.length < 11
+  return score
 end
 
 
 # make definition for tracking score
 def highest_score_from(words)
+  # create hash where all keys are valid words entered as key and the score
+  # of that word is stored as the value
   word_score_hash = {}
   tied_words = []
+  words.each_with_index { |word, i| word_score_hash[word] =  score_word(word) }
+  # find max score within the hash and assign it to 'max'
+  max = word_score_hash.max_by { |k,v| v }[1]
+  # find potential ties by selecting all keys (words) in the hash that have
+  # a value eqaul to 'max' --> store into an array
+  word_score_hash.each { |k, v| tied_words << k if v == max }
 
-  words.each_with_index do |word, i|
-    word_score_hash[word] =  score_word(word)
-  end
-
-  max = word_score_hash.max_by { |k,v| v  }[1]
-  # max = max[1]
-
-  word_score_hash.each do |k, v|
-   if v == max
-     tied_words << k
-   end
-end
-
-# finds and prints winner
-
-winning_hash = {}
-winning_word = tied_words.select { |word| word.length == 10 }
+  # finds and prints winner 10 letter words auto win, else shortest word wins
+  winning_hash = {}
+  winning_word = tied_words.select { |word| word.length == 10 }
   if tied_words.any? { |word| word.length == 10  }
     winning_hash[:word] = winning_word[0]
     winning_hash[:score] = max
@@ -142,11 +130,9 @@ end
 
 # validates word in dictionary
 def is_in_english_dict?(input)
-  dictionary_array = CSV.read('assets/dictionary-english.csv', headers: true).map  do |word|
-      word.to_h
-  end
- return dictionary_array.any? { |word| word['Word'] == input.downcase}
+  # create hash of verified words
+  dictionary_array = CSV.read('assets/dictionary-english.csv', headers: true)\
+  .map { |word| word.to_h }
+  # check if user entered word is in dictionary hash
+  return dictionary_array.any? { |word| word['Word'] == input.downcase}
 end
-
-# p is_in_english_dict?('jhfgkuftfuytdy')
-# p highest_score_from(['aaaaaaaaaa', 'z', 'buck'])
